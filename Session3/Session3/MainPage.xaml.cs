b787fc89e8a0bccf378504877457ff34c6afc358
KLHackathon <hackathon@kerrylogistics.com>
2019-01-25 21:31:09 +0800

    session3

---
+++ Session3/Session3/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DJI.WindowsSDK;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Session3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DJISDKManager.Instance.SDKRegistrationStateChanged += async (state, result) =>
+            {
+                var msg = result == SDKError.NO_ERROR? "Registration success" : result.ToString();
+                var prompt = new MessageDialog(msg);
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () => await prompt.ShowAsync());
+            };
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            DJISDKManager.Instance.RegisterApp(key.Text);
+        }
+    }
+}
